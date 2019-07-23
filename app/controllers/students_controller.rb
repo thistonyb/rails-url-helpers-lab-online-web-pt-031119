fa108327d318faa492ea27f6cf7049c6235f1b5e
Tony Baker <this.tonybaker@gmail.com>
2019-07-23 01:20:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,13 @@ class StudentsController < ApplicationController
     @students = Student.find_by(params[:id])
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to  student_path
+  end
+
   private
 
     def set_student
