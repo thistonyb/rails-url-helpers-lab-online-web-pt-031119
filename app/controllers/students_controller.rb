96983c5801c69345cb798235eda2573c65954f72
Tony Baker <this.tonybaker@gmail.com>
2019-07-23 02:07:13 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,7 +6,7 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @students = Student.find_by(params[:id])
+    @student = Student.find_by(params[:id])
   end
 
   def activate
